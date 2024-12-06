6f4f327924ab295fd69b6c2d6507cb86df3cc7f8
kamohelomokhethi <kamohelomokhethi25@gmail.com>
2024-12-06 15:45:20 +0200

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; 
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import './assets/style.css';
 import App from './App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='/AWY-human-resource-management-system'> 
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
