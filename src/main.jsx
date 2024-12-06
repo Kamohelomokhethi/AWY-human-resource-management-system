53b4b0ecf5046bb062bd0a05dd8a2b706a228a4e
Kamohelo Mokhethi <81868796+Kamohelomokhethi@users.noreply.github.com>
2024-12-06 14:46:54 +0200

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/AWY-human-resource-management-system'>
     <App />
   </BrowserRouter>
 );
