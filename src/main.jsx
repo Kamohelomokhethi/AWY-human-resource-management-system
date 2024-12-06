379f840bfa4eafcabe9b07e1e5cbb997b35b5112
Kamohelo Mokhethi <81868796+Kamohelomokhethi@users.noreply.github.com>
2024-12-06 15:45:54 +0200

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; 
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import './assets/style.css';
 import App from './App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
