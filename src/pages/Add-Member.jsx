966d4974b78c20f9d3b4829b005ccbdf2c03bea4
Kamohelo Mokhethi <81868796+Kamohelomokhethi@users.noreply.github.com>
2024-12-07 03:33:12 +0200

    all

--- src/pages/Add-Member.jsx
+++ src/pages/Add-Member.jsx
@@ -1,53 +1,97 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-const Sidebar = () => {
-   
+const AddVehicle = () => {
+  // Form state
+  const [formData, setFormData] = useState({
+    vin: '',
+    model: '',
+    mileage: '',
+  });
+
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const renderAnimatedLabel = (text) => {
+    return text.split('').map((letter, i) => (
+      <span key={i} style={{ transitionDelay: `${i * 50}ms` }}>
+        {letter}
+      </span>
+    ));
+  };
+
+  // Handle input change
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const response = await axios.post('http://localhost:5555/subscribe', formData);
+      enqueueSnackbar(response.data.message, { variant: 'success' });
+      navigate('/');
+    } catch (error) {
+      console.error('There was an error!', error);
+      enqueueSnackbar('Subscription failed.', { variant: 'error' });
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
-   <>
-        <h1>AWY HRMS</h1>
-        <br />
-        <h2>Dashboard</h2>
-        <br />
-        <div className="container">
-          <h2>Overview</h2>
-          <div className="cards">
-            <div className="card">
-              <div className="total">
-                <h3>Total Employees</h3>
-                <p>10</p>
-              </div>
+    <div className="form-container">
+      {isLoading && <div className="animate-ping w-16 h-16 m-8 rounded-full bg-sky-600"></div>}
+      <form onSubmit={handleSubmit}>
+        <h2>Add New Vehicle</h2>
 
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-240v-32q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v32q0 33-23.5 56.5T720-160H240q-33 0-56.5-23.5T160-240Zm80 0h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg>
-         
-            </div>
-
-            <div className="card">
-              <div className="total">
-                <h3>Training Session</h3>
-                <p className="name">Title: Lorem</p>
-                <p className="name">Type: Workshop</p>
-                <p className="name">Trainer: John Doe</p>
-              </div>
-               <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-40q0-17 11.5-28.5T280-880q17 0 28.5 11.5T320-840v40h320v-40q0-17 11.5-28.5T680-880q17 0 28.5 11.5T720-840v40h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Zm280 240q-17 0-28.5-11.5T440-440q0-17 11.5-28.5T480-480q17 0 28.5 11.5T520-440q0 17-11.5 28.5T480-400Zm-160 0q-17 0-28.5-11.5T280-440q0-17 11.5-28.5T320-480q17 0 28.5 11.5T360-440q0 17-11.5 28.5T320-400Zm320 0q-17 0-28.5-11.5T600-440q0-17 11.5-28.5T640-480q17 0 28.5 11.5T680-440q0 17-11.5 28.5T640-400ZM480-240q-17 0-28.5-11.5T440-280q0-17 11.5-28.5T480-320q17 0 28.5 11.5T520-280q0 17-11.5 28.5T480-240Zm-160 0q-17 0-28.5-11.5T280-280q0-17 11.5-28.5T320-320q17 0 28.5 11.5T360-280q0 17-11.5 28.5T320-240Zm320 0q-17 0-28.5-11.5T600-280q0-17 11.5-28.5T640-320q17 0 28.5 11.5T680-280q0 17-11.5 28.5T640-240Z"/></svg>
-          
-            </div>
-
-            <div className="card">
-              <div className="total">
-                <h3>Procurement</h3>
-                <p className="name">Total Cars: 12</p>
-                <p className="name">Available: 5</p>
-                <p className="name">In Service: 7</p>
-              </div>
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M240-200v40q0 17-11.5 28.5T200-120h-40q-17 0-28.5-11.5T120-160v-320l84-240q6-18 21.5-29t34.5-11h440q19 0 34.5 11t21.5 29l84 240v320q0 17-11.5 28.5T800-120h-40q-17 0-28.5-11.5T720-160v-40H240Zm-8-360h496l-42-120H274l-42 120Zm-32 80v200-200Zm100 160q25 0 42.5-17.5T360-380q0-25-17.5-42.5T300-440q-25 0-42.5 17.5T240-380q0 25 17.5 42.5T300-320Zm360 0q25 0 42.5-17.5T720-380q0-25-17.5-42.5T660-440q-25 0-42.5 17.5T600-380q0 25 17.5 42.5T660-320Zm-460 40h560v-200H200v200Z"/></svg>
-         
-            </div>
-          </div>
+        <div className="input-box">
+          <input
+            type="text"
+            name="vin"
+            value={formData.vin}
+            onChange={handleChange}
+            required
+          />
+          <label>{renderAnimatedLabel('vin')}</label>
         </div>
-        </>
+
+        <div className="input-box">
+          <input
+            type="text"
+            name="model"
+            value={formData.model}
+            onChange={handleChange}
+            required
+          />
+          <label>{renderAnimatedLabel('model')}</label>
+        </div>
+
+        <div className="input-box">
+          <input
+            type="text"
+            name="mileage"
+            value={formData.mileage}
+            onChange={handleChange}
+            required
+          />
+          <label>{renderAnimatedLabel('mileage')}</label>
+        </div>
+
+        <div className="input-box">
+          <input disabled={isLoading} type="submit" value="ADD" />
+        </div>
+      </form>
+    </div>
   );
 };
 
-export default Sidebar;
+export default AddVehicle;
