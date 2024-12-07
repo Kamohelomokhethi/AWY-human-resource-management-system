781723c51a6e641e2367f7a3caa233cead608b52
kamohelomokhethi <kamohelomokhethi25@gmail.com>
2024-12-07 02:47:40 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import DeleteMember from './pages/DeleteMember'
 import SearchMember from './pages/SearchMember'
 import Index from './pages/Index'
@@ -8,26 +10,27 @@ import AddVehicle from './pages/Add-vehicle'
 import SearchCar from './pages/Search-car'
 import Training from './pages/Training-schedule'
 import Addmember from './pages/Add-member'
-import {Routes, Route} from 'react-router-dom'
 import UpdateMember from './pages/UpdateMember'
-import React from 'react'
+import NavbarSidebar from './pages/navbar'
 
 function App() {
   return (
-    <Routes>
-    <Route path='/' element={<Index/>}/>
-    <Route path='/home' element={<Index/>}/>
-    <Route path='/add-member' element={<Addmember/>}/>
-    <Route path='/updatemember' element={<UpdateMember/>}/>
-    <Route path='/searchmember' element={<SearchMember/>}/>
-    <Route path='/deletemember' element={<DeleteMember/>}/> 
-    <Route path='/track-dev' element={<TrackDev/>}/>
-    <Route path='/training-Schedule' element={<Training/>}/>
-    <Route path='/points-overview' element={<Points/>}/>
-    <Route path='/vehicle-status' element={<VehicleStatus/>}/>
-    <Route path='/add-vehicle' element={<AddVehicle/>}/>
-    <Route path='/search-car' element={<SearchCar/>}/>
-  </Routes>
+    <NavbarSidebar>
+      <Routes>
+        <Route path='/' element={<Index />} />
+        <Route path='/home' element={<Index />} />
+        <Route path='/add-member' element={<Addmember />} />
+        <Route path='/updatemember' element={<UpdateMember />} />
+        <Route path='/searchmember' element={<SearchMember />} />
+        <Route path='/deletemember' element={<DeleteMember />} /> 
+        <Route path='/track-dev' element={<TrackDev />} />
+        <Route path='/training-Schedule' element={<Training />} />
+        <Route path='/points-overview' element={<Points />} />
+        <Route path='/vehicle-status' element={<VehicleStatus />} />
+        <Route path='/add-vehicle' element={<AddVehicle />} />
+        <Route path='/search-car' element={<SearchCar />} />
+      </Routes>
+    </NavbarSidebar>
   )
 }
 
