fcb0f06b7c2e45c98c6c101f9beb621951fe916d
Kamohelo Mokhethi <81868796+Kamohelomokhethi@users.noreply.github.com>
2024-12-07 03:54:54 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/AWY-human-resource-management-system/", 
-  build: {
-    outDir: 'dist', 
-    assetsDir: 'assets', 
-  },
-  server: {
-    port: 5173, 
-    open: true,
-  },
+  base: "/", 
+ 
 })
